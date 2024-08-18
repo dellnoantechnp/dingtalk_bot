344111079ed9f2a2e2bb5c8146c85caf787fab51
dellnoantechnp <dellnoantechnp@gmail.com>
2024-08-19 01:26:34 +0800

    dev: 调通 STREAM 模式下回调的入口，主动投递的卡片，回调方式采用 STREAM 模式，并且 dingtalk_stream 注册对应的 CallBack TOPIC 为回调TOPIC
    Signed-off-by: dellnoantechnp <dellnoantechnp@gmail.com>

--- dingtalk_bot/customRobot/apps.py
+++ dingtalk_bot/customRobot/apps.py
@@ -23,13 +23,15 @@ class CustomrobotConfig(AppConfig):
         credential = dingtalk_stream.Credential(client_id=client_id, client_secret=client_secret)
         client = dingtalk_stream.DingTalkStreamClient(credential)
         logger = logging.getLogger()
-        logger.setLevel(7)
-        client.register_callback_handler(dingtalk_stream.chatbot.ChatbotMessage.TOPIC, self.EchoMarkdownHandler(logger=logger))
+        logger.setLevel("DEBUG")
+        #client.register_callback_handler(dingtalk_stream.chatbot.ChatbotMessage.TOPIC, self.EchoMarkdownHandler(logger=logger))
+        ## 注册回调事件的 TOPIC
+        client.register_callback_handler(dingtalk_stream.chatbot.ChatbotHandler.TOPIC_CARD_CALLBACK, self.EchoMarkdownHandler(logger=logger))
         t = threading.Thread(target=client.start_forever, name="dingtalk_stream")
         t.daemon = True
         threads.append(t)
         t.start()
-        loop = asyncio.get_event_loop()
+        # loop = asyncio.get_event_loop()
         # result = loop.run_until_complete(client.start())
         # loop.close()
 
@@ -40,11 +42,12 @@ class CustomrobotConfig(AppConfig):
                 self.logger = logger
 
         async def process(self, callback: dingtalk_stream.CallbackMessage):
+            ## Docs: https://open.dingtalk.com/document/orgapp/event-callback-card  事件回调的content
             incoming_message = dingtalk_stream.ChatbotMessage.from_dict(callback.data)
             text = 'echo received message:\n'
             text += '\n'.join(['> 1. %s' % i for i in incoming_message.text.content.strip().split('\n')])
             # 回复一个 markdown 卡片消息
-            #self.reply_markdown('dingtalk-tutorial-python', text, incoming_message)
+            self.reply_markdown('dingtalk-tutorial-python', text, incoming_message)
             # 回复一个普通文本消息
-            self.reply_text(text=text, incoming_message=incoming_message)
+            #self.reply_text(text=text, incoming_message=incoming_message)
             return AckMessage.STATUS_OK, 'OK'
